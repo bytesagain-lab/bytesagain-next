c3f7e955bc74b24a3a3f65cb996f5bcf9d332f8f
BytesAgain <loutai0307@gmail.com>
2026-04-25 23:13:34 +0800

    fix: include skills sitemap in sitemap index

--- app/sitemap-index.xml/route.ts
+++ app/sitemap-index.xml/route.ts
@@ -1,12 +1,22 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  const today = new Date().toISOString().split('T')[0]
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>https://bytesagain.com/sitemap.xml</loc>
-    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <lastmod>${today}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>https://bytesagain.com/skills-sitemap.xml</loc>
+    <lastmod>${today}</lastmod>
   </sitemap>
 </sitemapindex>`
-  return new NextResponse(xml, { headers: { 'Content-Type': 'application/xml' } })
+  return new NextResponse(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=86400, s-maxage=86400',
+    },
+  })
 }
